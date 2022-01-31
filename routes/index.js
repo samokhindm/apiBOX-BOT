3303f385333e6ec54f8374e8ee0d856254b9c833
samokhindm <samokhindm@gmail.com>
2022-01-31 17:34:17 +0300

    v2.1

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const Router = require('express')
 const router = new Router()
 
+const userRouter = require('./userRouter')
 const projectRouter = require('./projectRouter')
 
 router.use('/project', projectRouter)
+router.use('/user', userRouter)
 
 module.exports = router
--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -1,9 +1,11 @@
 const Router = require('express')
 const router = new Router()
 const projectController = require('../controllers/projectController')
+const checkRole = require('../middleware/checkRoleMidleware')
 
-
-router.get('/', projectController.getAll)
-//router.get('/:id', projectController.settings)
+router.post('/save', checkRole('ADMIN'), projectController.saveSettings)
+router.get('/getall', checkRole('ADMIN'), projectController.getAll)
+router.get('/:gss_id', projectController.getSettings)
+router.del('/:id', checkRole('ADMIN'), projectController.deletProject)
 
 module.exports = router
